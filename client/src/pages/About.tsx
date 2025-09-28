6e140093534dccfc13f4569f914ae14385d26f68
Ian Zhao <ian.z05@outlook.com>
2025-09-28 15:40:49 -0400

    Start about section

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,7 +1,19 @@
 import Section from "../components/section";
+import { Text } from "../components/text";
 
 function About() {
-  return <Section id="about" title="About Us" />;
+  return (
+    <Section id="about" title="About Us">
+      <div className="rounded-xl max-w-sm sm:max-w-3xl w-[85vw] mx-auto bg-black/60 backdrop-blur-2xl py-40 px-16 relative">
+        <Text size="xl" className="text-center leading-10">
+          Want to get into quant trading? Hear from students with real
+          experience, participate in games and competitions sponsored by top
+          quant firms, and connect with peers passionate about finance. Join
+          Waterloo Quant Club today!
+        </Text>
+      </div>
+    </Section>
+  );
 }
 
 export default About;
