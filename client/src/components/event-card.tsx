e4d1c8dec8e9184199e26ef6a78ce63aa7401c0c
Ian Zhao <ian.z05@outlook.com>
2025-09-28 15:02:29 -0400

    Events tweak

--- client/src/components/event-card.tsx
+++ client/src/components/event-card.tsx
@@ -2,11 +2,12 @@ import { Text } from "./text";
 
 interface EventCardProps {
   eventName: string;
-  dateTime: string;
-  location: string;
-  description: string;
+  dateTime?: string;
+  location?: string;
+  description?: string;
   imageUrl: string;
-  onSignUp?: () => void;
+  linkText?: string;
+  linkHref?: string;
 }
 
 function EventCard({
@@ -15,7 +16,8 @@ function EventCard({
   location,
   description,
   imageUrl,
-  onSignUp,
+  linkText,
+  linkHref,
 }: EventCardProps) {
   return (
     <div className="flex flex-col sm:flex-row bg-gradient-to-b from-[#171B1Fcc] to-[#392940cc] rounded-xl overflow-hidden max-w-sm sm:max-w-4xl w-[85vw] border border-[#818181] hover:shadow-[0_0_8px_4px_#EECFEF] transition-shadow duration-200 ease-out z-[50]">
@@ -30,26 +32,33 @@ function EventCard({
         <Text size="xl" className="font-semibold mb-2 sm:mb-3 sm:text-2xl">
           {eventName}
         </Text>
-        <Text className="font-medium mb-3 sm:mb-4">
-          {dateTime} | {location}
-        </Text>
-        <Text
-          size="sm"
-          className="mb-6 flex-1 sm:flex-none leading-relaxed text-left"
-        >
-          {description}
-        </Text>
-        <button
-          onClick={onSignUp}
-          className="flex items-center cursor-pointer group"
-        >
-          <Text className="font-medium group-hover:text-[#BB68C5]">
-            Sign up
+        {dateTime && location && (
+          <Text className="font-medium mb-3 sm:mb-4">
+            {dateTime} | {location}
           </Text>
-          <span className="ml-2 transform group-hover:translate-x-1 group-hover:text-[#BB68C5] transition-transform duration-200">
-            &rsaquo;
-          </span>
-        </button>
+        )}
+        {description && (
+          <Text
+            size="sm"
+            className="mb-6 flex-1 sm:flex-none leading-relaxed text-left"
+          >
+            {description}
+          </Text>
+        )}
+        {linkHref && linkText && (
+          <a
+            href={linkHref}
+            target="_blank"
+            className="flex items-center cursor-pointer group"
+          >
+            <Text className="font-medium group-hover:text-[#BB68C5]">
+              {linkText}
+            </Text>
+            <span className="ml-2 transform group-hover:translate-x-1 group-hover:text-[#BB68C5] transition-transform duration-200">
+              &rsaquo;
+            </span>
+          </a>
+        )}
       </div>
     </div>
   );
--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -3,8 +3,6 @@ import Section from "../components/section";
 import EventCard from "../components/event-card";
 
 function Events() {
-  const handleSignUp = () => {};
-
   return (
     <Section id="events" title="Upcoming Events">
       <Text size="xl" className="-mt-4 mb-10 text-center">
@@ -17,15 +15,12 @@ function Events() {
           location="MC 1234"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis enim tortor. Donec aliquet sapien sapien, eu vulputate lorem rhoncus ac."
           imageUrl="/hero-background.png"
-          onSignUp={handleSignUp}
+          linkText="More Info"
+          linkHref="#"
         />
         <EventCard
-          eventName="Second Event"
-          dateTime="November 1, 2025 7:00 PM"
-          location="DC 5678"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies, est in sodales efficitur, diam nisl mattis nunc, vitae efficitur sem turpis aliquam nisi."
+          eventName="Stay Tuned for More!"
           imageUrl="/hero-background.png"
-          onSignUp={handleSignUp}
         />
       </div>
     </Section>
