17b1e8b2dc431ef572babe807f55ede464884aee
Ian Zhao <ian.z05@outlook.com>
2025-10-02 16:03:23 -0400

    Revert "Links page"
    This reverts commit 9463d97d63d89cf35d492be6ff531e3600da2670.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
-import Home from "./pages/Home";
+import Hero from "./pages/Hero";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
+import { SECTIONS } from "./util/constants";
 
 function App() {
   return (
     <div className="overflow-x-hidden">
       <NavBar />
-      <Home />
+      <Hero />
+      {SECTIONS.map(({ id, component: Component }) => (
+        <Component key={id} />
+      ))}
       <Footer />
     </div>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import App from "./App.tsx";
-import Links from "./pages/Links.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/links",
-    element: <Links />,
-  },
-]);
-
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
