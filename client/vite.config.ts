b506755e485cb78986851d550498169c4743806f
pahu2353 <pahu2353@gmail.com>
2025-10-20 23:57:26 -0700

    Update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        404: './public/404.html'
+      }
+    }
+  }
 })
